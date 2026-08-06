95ca48700d3c53df0c65443be61fad7f4001435c
shivayogids <shivayogids5@gmail.com>
2026-08-06 15:56:00 +0530

    Fix ESLint issues and prepare frontend for deployment

--- frontend/src/landing_page/products/ProductPage.js
+++ frontend/src/landing_page/products/ProductPage.js
@@ -5,56 +5,60 @@ import LeftSection from "./LeftSection";
 import RightSection from "./RightSection";
 import Universe from "./Universe";
 
-import Navbar from "../Navbar";
-import Footer from "../Footer";
-
 function PricingPage() {
   return (
     <>
       <Hero />
+
       <LeftSection
         imageURL="media/images/kite.png"
         productName="Kite"
         productDesription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
-        tryDemo=""
-        learnMore=""
-        googlePlay=""
-        appStore=""
+        tryDemo="/"
+        learnMore="/product"
+        googlePlay="https://play.google.com/store"
+        appStore="https://www.apple.com/app-store/"
       />
+
       <RightSection
         imageURL="media/images/console.png"
         productName="Console"
         productDesription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
-        learnMore=""
+        learnMore="/product"
       />
+
       <LeftSection
         imageURL="media/images/coin.png"
         productName="Coin"
         productDesription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
-        tryDemo=""
-        learnMore=""
-        googlePlay=""
-        appStore=""
+        tryDemo="/"
+        learnMore="/product"
+        googlePlay="https://play.google.com/store"
+        appStore="https://www.apple.com/app-store/"
       />
+
       <RightSection
         imageURL="media/images/kiteconnect.png"
         productName="Kite Connect API"
-        productDesription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
-        learnMore=""
+        productDesription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our client base."
+        learnMore="/product"
       />
+
       <LeftSection
         imageURL="media/images/varsity.png"
-        productName="Varsity mobile"
-        productDesription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
-        tryDemo=""
-        learnMore=""
-        googlePlay=""
-        appStore=""
+        productName="Varsity Mobile"
+        productDesription="An easy-to-grasp collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-sized cards to help you learn on the go."
+        tryDemo="/"
+        learnMore="/product"
+        googlePlay="https://play.google.com/store"
+        appStore="https://www.apple.com/app-store/"
       />
+
       <p className="text-center mt-5 mb-5">
         Want to know more about our technology stack? Check out the Zerodha.tech
         blog.
       </p>
+
       <Universe />
     </>
   );
