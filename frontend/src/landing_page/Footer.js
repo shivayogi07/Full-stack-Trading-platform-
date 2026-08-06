e42e9a3e3714f5d6cf61cc2a2352d5cf511b6216
shivayogids <shivayogids5@gmail.com>
2026-08-06 15:35:09 +0530

    Fix Footer and Hero ESLint issues

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -6,95 +7,103 @@ function Footer() {
       <div className="container border-top mt-5">
         <div className="row mt-5">
           <div className="col">
-            <img src="media/images/logo.svg" style={{ width: "50%" }} />
+            <img
+              src="media/images/logo.svg"
+              alt="Zerodha Logo"
+              style={{ width: "50%" }}
+            />
+
             <p>
               &copy; 2010 - 2026, Not Zerodha Broking Ltd. All rights reserved.
             </p>
           </div>
+
           <div className="col">
             <p>Company</p>
-            <a href="">About</a>
+
+            <Link to="/about">About</Link>
             <br />
-            <a href="">Products</a>
+
+            <Link to="/product">Products</Link>
             <br />
-            <a href="">Pricing</a>
+
+            <Link to="/pricing">Pricing</Link>
             <br />
-            <a href="">Referral programme</a>
+
+            <Link to="/">Referral programme</Link>
             <br />
-            <a href="">Careers</a>
+
+            <Link to="/">Careers</Link>
             <br />
-            <a href="">Zerodha.tech</a>
+
+            <Link to="/">Zerodha.tech</Link>
             <br />
-            <a href="">Press & media</a>
+
+            <Link to="/">Press & media</Link>
             <br />
-            <a href="">Zerodha cares (CSR)</a>
+
+            <Link to="/">Zerodha cares (CSR)</Link>
             <br />
           </div>
+
           <div className="col">
             <p>Support</p>
-            <a href="">Contact</a>
+
+            <Link to="/support">Contact</Link>
             <br />
-            <a href="">Support portal</a>
+
+            <Link to="/support">Support portal</Link>
             <br />
-            <a href="">Z-Connect blog</a>
+
+            <Link to="/support">Z-Connect blog</Link>
             <br />
-            <a href="">List of charges</a>
+
+            <Link to="/support">List of charges</Link>
             <br />
-            <a href="">Downloads & resources</a>
+
+            <Link to="/support">Downloads & resources</Link>
             <br />
           </div>
+
           <div className="col">
             <p>Account</p>
-            <a href="">Open an account</a>
+
+            <Link to="/signup">Open an account</Link>
             <br />
-            <a href="">Fund transfer</a>
+
+            <Link to="/">Fund transfer</Link>
             <br />
-            <a href="">60 day challenge</a>
+
+            <Link to="/">60 day challenge</Link>
             <br />
           </div>
         </div>
+
         <div className="mt-5 text-muted" style={{ fontSize: "14px" }}>
           <p>
-            Zerodha Broking Ltd.: Member of NSE​ &​ BSE – SEBI Registration no.:
+            Zerodha Broking Ltd.: Member of NSE & BSE – SEBI Registration no.:
             INZ000031633 CDSL: Depository services through Zerodha Securities
             Pvt. Ltd. – SEBI Registration no.: IN-DP-100-2015 Commodity Trading
             through Zerodha Commodities Pvt. Ltd. MCX: 46025 – SEBI Registration
             no.: INZ000038238 Registered Address: Zerodha Broking Ltd.,
             #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School,
-            J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any
-            complaints pertaining to securities broking please write to
-            complaints@zerodha.com, for DP related to dp@zerodha.com. Please
-            ensure you carefully read the Risk Disclosure Document as prescribed
-            by SEBI | ICF
+            J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India.
           </p>
+
           <p>
             Procedure to file a complaint on SEBI SCORES: Register on SCORES
             portal. Mandatory details for filing complaints on SCORES: Name,
-            PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
-            Communication, Speedy redressal of the grievances
+            PAN, Address, Mobile Number, E-mail ID.
           </p>
 
           <p>
             Investments in securities market are subject to market risks; read
             all the related documents carefully before investing.
           </p>
+
           <p>
-            "Prevent unauthorised transactions in your account. Update your
-            mobile numbers/email IDs with your stock brokers. Receive
-            information of your transactions directly from Exchange on your
-            mobile/email at the end of the day. Issued in the interest of
-            investors. KYC is one time exercise while dealing in securities
-            markets - once KYC is done through a SEBI registered intermediary
-            (broker, DP, Mutual Fund etc.), you need not undergo the same
-            process again when you approach another intermediary." Dear
-            Investor, if you are subscribing to an IPO, there is no need to
-            issue a cheque. Please write the Bank account number and sign the
-            IPO application form to authorize your bank to make payment in case
-            of allotment. In case of non allotment the funds will remain in your
-            bank account. As a business we don't give stock tips, and have not
-            authorized anyone to trade on behalf of others. If you find anyone
-            claiming to be part of Zerodha and offering such services, please
-            create a ticket here.
+            Prevent unauthorised transactions in your account. Update your
+            mobile numbers/email IDs with your stock broker.
           </p>
         </div>
       </div>
--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,8 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
+
 function PricingPage() {
   return (
     <>
