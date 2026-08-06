4ae995a90f7b082ff3aad3abede29816a68c19b7
shivayogids <shivayogids5@gmail.com>
2026-08-06 16:02:28 +0530

    Fix ESLint issues and prepare frontend for deployment

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,23 +2,27 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="conatiner p-5">
+    <div className="container p-5">
       <div className="row text-center">
         <img
           src="media/images/homeHero.png"
-          alt="Hero image"
-          className="mb-5"
-        ></img>
-        <h1 className="mt-4"> Invest in everything</h1>
+          alt="Hero"
+          className="mb-5 img-fluid"
+        />
+
+        <h1 className="mt-4">Invest in Everything</h1>
+
         <p>
           Online platform to invest in stocks, derivatives, mutual funds, and
-          more
+          more.
         </p>
+
         <button
-          className="p-2 btn btn-primary fs-5 mb-5"
+          type="button"
+          className="btn btn-primary fs-5 p-2 mb-5"
           style={{ width: "20%", margin: "0 auto" }}
         >
-          Signup Now
+          Sign Up Now
         </button>
       </div>
     </div>
