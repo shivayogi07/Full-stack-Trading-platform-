e237d56e85fcf82c962efbb469d68813fb81f078
shivayogids <shivayogids5@gmail.com>
2026-08-06 15:53:38 +0530

    Fix ESLint issues and prepare frontend for deployment

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -5,47 +5,84 @@ function Universe() {
     <div className="container mt-5">
       <div className="row text-center">
         <h1>The Zerodha Universe</h1>
+
         <p>
           Extend your trading and investment experience even further with our
-          partner platforms
+          partner platforms.
         </p>
 
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" style={{ width: "35%" }} />
-          <p className="text-small text-muted mt-2">
-            Thematic investment platform
-          </p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media\images\streakLogo.png" style={{ width: "35%" }} />
-          <p className="text-small text-muted mt-2">
-            Thematic investment platform
-          </p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media\images\sensibullLogo.svg" style={{ width: "35%" }} />
-          <p className="text-small text-muted mt-3">Algo & strategy platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
           <img
-            src="media\images\zerodhaFundhouse.png"
+            src="media/images/smallcaseLogo.png"
+            alt="Smallcase"
             style={{ width: "35%" }}
           />
-          <p className="text-small text-muted mt-2">optins trading platform</p>
+          <p className="text-small text-muted mt-2">
+            Thematic investment platform
+          </p>
         </div>
+
         <div className="col-4 p-3 mt-5">
-          <img src="media\images\goldenpiLogo.png" style={{ width: "35%" }} />
-          <p className="text-small text-muted mt-2">Bonds trading platform</p>
+          <img
+            src="media/images/streakLogo.png"
+            alt="Streak"
+            style={{ width: "35%" }}
+          />
+          <p className="text-small text-muted mt-2">
+            Algo & strategy platform
+          </p>
         </div>
+
         <div className="col-4 p-3 mt-5">
-          <img src="media\images\dittoLogo.png" style={{ width: "35%" }} />
-          <p className="text-small text-muted mt-2">Ditto </p>
+          <img
+            src="media/images/sensibullLogo.svg"
+            alt="Sensibull"
+            style={{ width: "35%" }}
+          />
+          <p className="text-small text-muted mt-2">
+            Options trading platform
+          </p>
         </div>
+
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/images/zerodhaFundhouse.png"
+            alt="Zerodha Fund House"
+            style={{ width: "35%" }}
+          />
+          <p className="text-small text-muted mt-2">
+            Mutual fund platform
+          </p>
+        </div>
+
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/images/goldenpiLogo.png"
+            alt="GoldenPi"
+            style={{ width: "35%" }}
+          />
+          <p className="text-small text-muted mt-2">
+            Bonds investment platform
+          </p>
+        </div>
+
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/images/dittoLogo.png"
+            alt="Ditto"
+            style={{ width: "35%" }}
+          />
+          <p className="text-small text-muted mt-2">
+            Insurance platform
+          </p>
+        </div>
+
         <button
-          className="p-2 btn btn-primary fs-5 mb-5"
+          type="button"
+          className="btn btn-primary fs-5 p-2 mb-5"
           style={{ width: "20%", margin: "0 auto" }}
         >
-          Signup Now
+          Sign Up Now
         </button>
       </div>
     </div>
