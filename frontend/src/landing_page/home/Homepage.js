788548892b15f5684823771a73de9d7edb2db4cb
shivayogids <shivayogids5@gmail.com>
2026-08-06 16:00:17 +0530

    Fix ESLint issues and prepare frontend for deployment

--- frontend/src/landing_page/home/Homepage.js
+++ frontend/src/landing_page/home/Homepage.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import Awards from './Awards';
-import Stats from './Stats';
-import Pricing from './Pricing';
-import Education from './Education';
-import OpenAccount from '../OpenAccount';
-import Hero from './Hero';
-import NavBar from '../Navbar';
-import Footer from '../Footer';
+import React from "react";
+
+import Awards from "./Awards";
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Education from "./Education";
+import OpenAccount from "../OpenAccount";
+import Hero from "./Hero";
 
 function HomePage() {
-    return ( 
-        <>
-            
-            <Hero></Hero>
-            <Awards></Awards>
-            <Stats></Stats>
-            <Pricing></Pricing>
-            <Education></Education>
-            <OpenAccount></OpenAccount>
-            
-
-        </>
-     );
-
+  return (
+    <>
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+    </>
+  );
 }
 
 export default HomePage;
